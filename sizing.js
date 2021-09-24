78be84e1871da42c1a519c58d789081c3ac641f0
Liam Doughty <liamdoughty5@gmail.com>
2021-09-23 21:10:13 -0400

    got height of screen and height of specific element. should add in feature to adjust scaling to screen height and width

--- sizing.js
+++ sizing.js
@@ -1,8 +1,14 @@
 var screenHeight = window.innerHeight;
 var screenWidth = window.innerWidth;
 
+console.log( 'height of the current display: ' +screenHeight );
+
 if( innerHeight < 1000 ) {
 
- /* screen size is small */
+    var content = document.querySelector( '.top_text_container' );
+    let contentHeight = content.offsetHeight;
+    content.setAttribute( "style", "transform: scale( 0.8 );" );
+
+    console.log( 'contentHeight: ' +contentHeight );
 
 }

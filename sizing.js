1cff4bc26dbaeaa07bf74f08f7565ed5314bd533
Liam Doughty <liamdoughty5@gmail.com>
2021-09-25 14:12:00 -0400

    added scaling feature for card container so both content and cards will automatically height scale when the browser window size changes

--- sizing.js
+++ sizing.js
@@ -4,22 +4,35 @@ var screenWidth = window.innerWidth;
 
 var contentContainer = document.querySelector( '.top_text_container' );
 let contentContainerHeight = contentContainer.offsetHeight;
-let contentContainerWidth = contentContainer.offsetWidth;
-let scalingFactor = (screenHeight/contentContainerHeight) - (screenHeight/950);
-scalingFactor = scalingFactor.toFixed(2);
+let contentScalingFactor = (screenHeight/contentContainerHeight) - (screenHeight/950);
+contentScalingFactor = contentScalingFactor.toFixed(2);
 
-contentContainer.style.transform = "scale(" +scalingFactor+ ")";
+var cardContainer = document.querySelector( '.card_container' );
+let cardHeight = cardContainer.offsetHeight;
+let cardScalingFactor = 0.84*(screenHeight/cardHeight);
+cardScalingFactor = cardScalingFactor.toFixed(2);
+
+console.log( cardScalingFactor );
+
+
+contentContainer.style.transform = "scale(" +contentScalingFactor+ ")";
+
 
 window.addEventListener( 'resize', function() {
 
     screenHeight = window.innerHeight;
     screenWidth = window.innerWidth;
     
+
     if( screenHeight > 700 ) {
         
-        scalingFactor = (screenHeight/contentContainerHeight) - (screenHeight/950);
-        scalingFactor = scalingFactor.toFixed(2);
-        contentContainer.style.transform = "scale(" +scalingFactor+  ")";
+        contentScalingFactor = (screenHeight/contentContainerHeight) - (screenHeight/950);
+        contentScalingFactor = contentScalingFactor.toFixed(2);
+        contentContainer.style.transform = "scale(" +contentScalingFactor+  ")";
+
+        cardScalingFactor = 0.84*(screenHeight/cardHeight);
+        cardScalingFactor = cardScalingFactor.toFixed(2);
+        cardContainer.style.transform = "scale("+cardScalingFactor+")";
 
     }
 
